38422902c3f39beb0367c888b9ee751b3ebb5cfe
João Amadeu <jmamadeu2000@gmail.com>
2021-10-21 08:14:05 +0100

    chore: add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import 'antd/dist/antd.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import { Provider as ReduxProvider } from 'react-redux'
+
+import { store } from './app/store'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
